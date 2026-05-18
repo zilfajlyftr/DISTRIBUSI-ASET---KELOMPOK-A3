c00d900736944d84e69d7e7ac4b619576085bb53
Zilfa <zilfajlyftri@gmail.com>
2026-05-18 19:39:37 +0700

    implementasi ACL

--- middlewares/authMiddleware.js
+++
@@ -1,8 +0,0 @@
-const isAuthenticated = (req, res, next) => {
-  if (req.session && req.session.user) {
-    return next();
-  }
-  res.redirect('/login');
-};
-
-module.exports = { isAuthenticated };
